dd00953bab217082154d008871774a5e741042cf
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2024-07-25 19:38:22 -0400

    add path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@ import sys
 import pathlib
 # worked
 # sys.path.insert(0, os.path.abspath('../../../GaussParse/'))
-sys.path.insert(0, os.path.abspath('../GaussParse'))
+sys.path.insert(0, os.path.abspath('../..'))
 # sys.path.insert(0, os.path.abspath('../../../../GaussParse/'))
 
 # path = pathlib.Path(__file__).resolve() / '..' / '..' / 'GaussParse'
