25ac1771f547452cc09311d9bb2ba3452de14a4c
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2024-07-25 16:14:13 -0400

    '../../../GaussParse/'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,8 +9,8 @@ import sys
 import pathlib
 
 # worked
-# sys.path.insert(0, os.path.abspath('../../../GaussParse/'))
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../../../GaussParse/'))
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
