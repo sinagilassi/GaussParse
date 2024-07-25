bb1be872e64a1732b4caa8143fa7f2c1bb0bcaca
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2024-07-25 17:47:18 -0400

    add path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@ import sys
 
 # worked
 # sys.path.insert(0, os.path.abspath('../../../GaussParse/'))
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../'))
 # sys.path.insert(0, os.path.abspath('../../GaussParse/'))
 
 
