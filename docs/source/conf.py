2b49d1b53dab5bf6f80ebf5b8655c38393f85a74
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2024-07-25 20:03:46 -0400

    add p

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,8 +8,8 @@ import os
 import sys
 import pathlib
 # worked
-sys.path.insert(0, os.path.abspath('../GaussParse'))
-# sys.path.insert(0, os.path.abspath('../..'))
+# sys.path.insert(0, os.path.abspath('../GaussParse'))
+sys.path.insert(0, os.path.abspath('..'))
 # sys.path.insert(0, os.path.abspath('../../../../GaussParse/'))
 
 # path = pathlib.Path(__file__).resolve() / '..' / '..' / 'GaussParse'
