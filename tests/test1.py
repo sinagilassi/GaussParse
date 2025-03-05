039b731488562c5fd07b18f3c20efdc1701f9653
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-03-04 21:28:50 -0500

    update test

--- tests/test1.py
+++ tests/test1.py
@@ -14,7 +14,7 @@ gp.app_info()
 # xls_file = 'D:\\OneDrive\\Project Analysis\\Computational Chemistry\\analysis\\terpens epoxidation\\extract data\\acetone-limonene-mechanism-1-b3lyp.xlsx'
 # xls_file = 'D:\\OneDrive\\Project Analysis\\Computational Chemistry\\analysis\\terpens epoxidation\\extract data\\acetone-cis-3-Hexene-mechanism-1.xlsx'
 
-xls_file = 'D:\\OneDrive\\Project Analysis\\Computational Chemistry\\analysis\\terpens epoxidation\\extract data\\acetone-TS comparison-B3LYP.xlsx'
+xls_file = 'D:\\OneDrive\\Project Analysis\\Computational Chemistry\\analysis\\terpens epoxidation\\extract data\\acetone-irc-B3LYP.xlsx'
 # log files
 # gp.collect_files_from(xls_file, sheet_name="Sheet1")
 # txt files
@@ -49,6 +49,10 @@ gaussian_log_folder = 'D:\\OneDrive\\Project Analysis\\Computational Chemistry\\
 # gaussian_log = 'D:\\OneDrive\\Project Analysis\\Computational Chemistry\\analysis\\terpens epoxidation\\extract data\\frustose-TS2-data'
 # file
 # gaussian_log = r"D:\OneDrive\Project Analysis\Computational Chemistry\models\terpens epoxidation\limonene-dmdo with fructose keton\mechansim 3\step 2\qst3-2\res 1\tight\res 1\g09_exp.log"
+
+# folder
+gaussian_log = 'D:\\OneDrive\\Project Analysis\\Computational Chemistry\\analysis\\terpens epoxidation\\extract data\\acetone-limonene-mechanism-2-B3LYP-6-311-g(3df-2p)'
+
 # save in txt
 # gp.input_orientation_to_txt(gaussian_log)
 
@@ -68,18 +72,23 @@ gaussian_log_folder = 'D:\\OneDrive\\Project Analysis\\Computational Chemistry\\
 plt_data = 'D:\\OneDrive\\Project Analysis\\Computational Chemistry\\analysis\\terpens epoxidation\\extract data\\acetone-limonene-mechanism-2-B3LYP-6-311-g(3df-2p)\\energy.xlsx'
 
 # plot energy profile and save it
-gp.plot_energy_profile(plt_data)
+# gp.plot_energy_profile(plt_data)
 
 # *******************************
 # Save IRC profile
 # *******************************
 # file path
 # plt_data = "D:\\OneDrive\\Project Analysis\\Computational Chemistry\\analysis\\terpens epoxidation\\extract data\\acetone-irc-data\\trans-TS1.log"
+
 # folder path
 # plt_data = "D:\\OneDrive\\Project Analysis\\Computational Chemistry\\analysis\\terpens epoxidation\\irc"
 # plt_data = "D:\\OneDrive\\Project Analysis\\Computational Chemistry\\analysis\\terpens epoxidation\\extract data\\fructose-irc-data"
+plt_data = "D:\\OneDrive\\Project Analysis\\Computational Chemistry\\analysis\\terpens epoxidation\\extract data\\acetone-irc-B3LYP"
+
 # save IRC profile
 # manual_options = {
 #     "y_unit": "kcal/mol"
 # }
+
+# execute
 # gp.plot_irc_profile(plt_data)
